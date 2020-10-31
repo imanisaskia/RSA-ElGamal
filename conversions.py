94d6fcdc30e57946805c924be95cfabf47f5d0bc
Saskia Imani <imani.saskia@gmail.com>
2020-10-31 21:15:32 +0700

    Conversion from int blocks to string

--- conversions.py
+++ conversions.py
@@ -13,8 +13,20 @@ def byteToInt(b):
     return int.from_bytes(b, "big")
 
 def get_blocks(message, n):
-    msg_bytes = str(byteToInt(strToByte(message)))
+    msg_int_string = str(byteToInt(strToByte(message)))
     size = len(str(n)) - 1
-    return [int(msg_bytes[i : i + size]) for i in range(0, len(msg_bytes), size)]
+    return [int(msg_int_string[i : i + size]) for i in range(0, len(msg_int_string), size)]
 
-print(get_blocks("HELLO ALICE", 105))
+def get_string(blocks, n):
+    msg_int_string = ""
+    size = len(str(n)) - 1
+    for block in blocks:
+        digits = len(str(block))
+        msg_int_string += ('0' * (size - digits)) + str(block)
+    return byteToStr(intToByte(int(msg_int_string)))
+
+blocks = get_blocks("HELLO ALICE", 105)
+print(blocks)
+
+msg = get_string(blocks, 105)
+print(msg)
