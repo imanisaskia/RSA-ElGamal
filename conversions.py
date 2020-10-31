3b6a2c9ddb41e4b02dce76ae1e15c56bb5ad8af4
Saskia Imani <imani.saskia@gmail.com>
2020-10-31 21:06:28 +0700

    Conversion.py changes

--- conversions.py
+++ conversions.py
@@ -14,21 +14,7 @@ def byteToInt(b):
 
 def get_blocks(message, n):
     msg_bytes = str(byteToInt(strToByte(message)))
-    size = get_block_size(n) - 1
+    size = len(str(n)) - 1
     return [int(msg_bytes[i : i + size]) for i in range(0, len(msg_bytes), size)]
-
-def get_block_size(n):
-    max_val = n-1
-    size = 1
-    while max_val > 9:
-        max_val = max_val // 10
-        size += 1
-    return size
-
-def arr_to_str(arr):
-    try:
-        return ''.join(str(e) for e in arr)
-    except:
-        return ''.join(arr)
 
 print(get_blocks("HELLO ALICE", 105))
