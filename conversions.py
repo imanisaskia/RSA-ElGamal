5643a54b036342a88dfdafff610a4e11c5954904
Saskia Imani <imani.saskia@gmail.com>
2020-11-01 05:02:39 +0700

    Added comments

--- conversions.py
+++ conversions.py
@@ -1,5 +1,6 @@
 from Crypto.Util.number import bytes_to_long, long_to_bytes
 
+# from string (plaintext) to blocks of BigNum
 def string_get_blocks(message, block_size):
     blocks = []
     for i in range(0, len(message), block_size):
@@ -7,6 +8,7 @@ def string_get_blocks(message, block_size):
         blocks.append(block)
     return blocks
 
+# from integer string (ciphertext) to blocks of BigNum
 def int_get_blocks(message, block_size):
     blocks = []
     for i in range(0, len(message), block_size):
@@ -14,6 +16,7 @@ def int_get_blocks(message, block_size):
         blocks.append(block)
     return blocks
 
+# from blocks of BigNum to integer string (ciphertext)
 def get_int(blocks, max_digits):
     int_string = ''
     for block in blocks:
@@ -23,6 +26,7 @@ def get_int(blocks, max_digits):
         int_string += string
     return int_string
 
+# from blocks of BigNum to string (plaintext)
 def get_string(blocks):
     message = long_to_bytes(blocks[0])
     for i in range(1, len(blocks)):
